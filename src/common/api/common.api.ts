eb4f084388c2e7c09aeab02a7c257b3dd3132353
jalxby <jalxby@gmail.com>
2023-06-01 06:36:19 +0200

    password recovery feature - no blank after deploy!!

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -13,15 +13,15 @@ export const instancePassRestore = axios.create({
   baseURL: "https://neko-back.herokuapp.com/2.0/",
   withCredentials: true,
 });
-instance.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  function (error) {
-    if (error.response.status === 401 && globalRouter.navigate) {
-      globalRouter.navigate("/signin");
-    }
-    console.log("from interceptor", error);
-    return Promise.reject(error);
-  }
-);
+// instance.interceptors.response.use(
+//   function (response) {
+//     return response;
+//   },
+//   function (error) {
+//     if (error.response.status === 401 && globalRouter.navigate) {
+//       globalRouter.navigate("/signin");
+//     }
+//     console.log("from interceptor", error);
+//     return Promise.reject(error);
+//   }
+// );
