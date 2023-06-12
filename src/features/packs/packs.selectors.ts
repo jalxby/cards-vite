da20043d1312d785cd7b3dfd5f1a14b98cbe9326
jalxby <jalxby@gmail.com>
2023-06-12 16:14:02 +0200

    refactoring Table

--- src/features/packs/packs.selectors.ts
+++ src/features/packs/packs.selectors.ts
@@ -1,4 +1,6 @@
 import { RootState } from "@/app/store.ts";
+import { createSelector } from "@reduxjs/toolkit";
+import { formatDate } from "@/common/utils/formatDate.ts";
 
 export const selectTotalPacks = (state: RootState) =>
   state.packs.cardsData.cardPacksTotalCount;
@@ -13,3 +15,10 @@ export const selectPackTitle = (state: RootState) => (id: string) =>
   state.packs.cardsData.cardPacks.filter((pack) => pack._id === id)[0].name;
 export const selectIsPrivate = (state: RootState) => (id: string) =>
   state.packs.cardsData.cardPacks.filter((pack) => pack._id === id)[0].private;
+
+export const selectPacksFormatDate = createSelector([selectPacks], (packs) => {
+  return packs.map((pack) => ({
+    ...pack,
+    updated: formatDate(new Date(pack.updated)),
+  }));
+});
--- src/features/packs/packsTable/PacksTable.tsx
+++ src/features/packs/packsTable/PacksTable.tsx
@@ -1,12 +1,16 @@
 import { useAppSelector } from "@/common/hooks/hooks.ts";
 import { formatDate } from "@/common/utils/formatDate.ts";
 import { AllowedActs } from "@/features/packs/AllowedActs.tsx";
-import { selectPacks } from "@/features/packs/packs.selectors.ts";
+import {
+  selectPacks,
+  selectPacksFormatDate,
+} from "@/features/packs/packs.selectors.ts";
 import { Skeleton, Table } from "@mantine/core";
 import React from "react";
 import { selectIsLoading } from "@/app/app.selectors.ts";
 import { ColumnHeader } from "@/features/packs/ColumnHeader.tsx";
 import s from "./PacksTable.module.scss";
+import { PackType } from "@/features/packs/packs.api.ts";
 
 export enum Column {
   NAME = "Name",
@@ -26,61 +30,54 @@ export const CurrentSorted: Record<string, ValuesToSort> = {
 };
 
 export const PacksTable = React.memo(() => {
-  const packs = useAppSelector(selectPacks);
+  const packs = useAppSelector(selectPacksFormatDate);
   const isLoading = useAppSelector(selectIsLoading);
-  const columns = Array.from(Object.values(Column)).map((column_name) => {
+  const thead_columns = Array.from(Object.values(Column)).map((column_name) => {
     return (
       <th>
         <ColumnHeader key={column_name} title={column_name} />
       </th>
     );
   });
+
+  const rows = packs.map(
+    ({ _id, name, cardsCount, updated, user_name, user_id }) => (
+      <tr key={_id}>
+        <td>
+          <Skeleton visible={isLoading} animate={false}>
+            {name}
+          </Skeleton>
+        </td>
+        <td>
+          <Skeleton visible={isLoading} animate={false}>
+            {cardsCount}
+          </Skeleton>
+        </td>
+        <td>
+          <Skeleton visible={isLoading} animate={false}>
+            {updated}
+          </Skeleton>
+        </td>
+        <td>
+          <Skeleton visible={isLoading} animate={false}>
+            {user_name}
+          </Skeleton>
+        </td>
+        <td>
+          <AllowedActs user_id={user_id} pack_id={_id} />
+        </td>
+      </tr>
+    )
+  );
+
+  const tbody_rows = packs.length !== 0 ? rows : <>{"NO PACKS FOUND"}</>;
+
   return (
     <Table withBorder>
       <thead className={s.thead}>
-        <tr>{columns}</tr>
+        <tr>{thead_columns}</tr>
       </thead>
-      <tbody className={s.tbody}>
-        {packs.length !== 0 ? (
-          packs.map((element) => {
-            const date = formatDate(new Date(element.updated));
-            return (
-              <tr key={element._id}>
-                <td>
-                  <Skeleton visible={isLoading} animate={false}>
-                    {element.name}
-                  </Skeleton>
-                </td>
-                <td>
-                  <Skeleton visible={isLoading} animate={false}>
-                    {element.cardsCount}
-                  </Skeleton>
-                </td>
-                <td>
-                  <Skeleton visible={isLoading} animate={false}>
-                    {date}
-                  </Skeleton>
-                </td>
-                <td>
-                  <Skeleton visible={isLoading} animate={false}>
-                    {element.user_name}
-                  </Skeleton>
-                </td>
-                <td>
-                  {
-                    <AllowedActs
-                      user_id={element.user_id}
-                      pack_id={element._id}
-                    />
-                  }
-                </td>
-              </tr>
-            );
-          })
-        ) : (
-          <>{"NO PACKS FOUND"}</>
-        )}
-      </tbody>
+      <tbody className={s.tbody}>{tbody_rows}</tbody>
     </Table>
   );
 });
