ab8b528c9c0e71e451193bafeb2f7c49ad440117
jalxby <jalxby@gmail.com>
2023-06-20 02:08:49 +0200

    Avatar add file icon button

--- src/common/components/editableAvatar/Avatar.tsx
+++ src/common/components/editableAvatar/Avatar.tsx
@@ -1,17 +1,33 @@
 import { CameraIcon } from "@/assets/CameraIcon.tsx";
-import { Avatar } from "@mantine/core";
-import React from "react";
-import s from "./EditableAvatar.module.scss";
 import { myAvatar } from "@/assets/myAvatar.ts";
+import { Avatar } from "@mantine/core";
+import React, { ChangeEvent, useRef } from "react";
+import s from "./EditableAvatar.module.scss";
 
 export const EditableAvatar = () => {
-  const onClick = () => {
-    console.log("edit mode avatar!");
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleButtonClick = () => {
+    ref.current?.click();
   };
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const selectedFile = event.target.files?.[0];
+    // Здесь вы можете выполнить дополнительные действия с выбранным файлом, например, загрузить его на сервер или выполнить другую обработку.
+
+    console.log("Выбранный файл:", selectedFile);
+  };
+
   return (
     <div className={s.container}>
       <Avatar variant="outline" radius="50%" size="xl" src={myAvatar} />
-      <div onClick={onClick} className={s.icon}>
+      <input
+        type="file"
+        ref={ref}
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+      <div onClick={handleButtonClick} className={s.icon}>
         <CameraIcon />
       </div>
     </div>
